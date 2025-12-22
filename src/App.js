b176d983020a07e42d58c5b32f85861c76ad658d
Tatul Minasyan <a1234@MacBook-Air-15.local>
2025-12-22 22:05:45 +0400

    url changed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from './Pages/Home/Home'
-import { Routes, HashRouter, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './Pages/Login/Login'
 import Player from './Pages/Player/Player';
 import { onAuthStateChanged } from 'firebase/auth';
@@ -28,13 +28,11 @@ export function App() {
   return (
     <div className="App">
       <ToastContainer theme='dark' />
-      <HashRouter>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/player/:id' element={<Player />} />
       </Routes>
-      </HashRouter>
     </div>
   );
 }
