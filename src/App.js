fb602c765032e00b0b2b312fd18802a0ddacca59
Tatul Minasyan <a1234@MacBook-Air-15.local>
2025-12-23 23:56:17 +0400

    fixed bug4

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import Home from './Pages/Home/Home'
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './Pages/Login/Login'
 import Player from './Pages/Player/Player';
-import { onAuthStateChanged } from 'firebase/auth';
-import { useEffect } from 'react';
-import { auth } from './firebase';
 import { ToastContainer } from 'react-toastify';
 
 
